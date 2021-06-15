4c93964534aa0845e5d71a93dc6db788834c9133
kittipoom.p <kittipoom@feyverly.com>
2021-06-15 21:32:41 +0700

    web socket

---
+++ MessengerService/Handlers/WebSocketMessageHandler.cs
@@ -0,0 +1,31 @@
+﻿using MessengerService.SocketManager;
+using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerService.Handlers
+{
+    public class WebSocketMessageHandler : SocketHandler
+    {
+        public WebSocketMessageHandler(ConnectionManager connection) : base(connection)
+        {
+
+        }
+
+        public override async Task OnConnected(WebSocket socket)
+        {
+            await base.OnConnected(socket);
+            var socketId = Connection.GetId(socket);
+            await SendMessageToAll($"{socketId} just joined the party *****");
+        }
+
+        public override async Task Receive(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            var socketId = Connection.GetId(socket);
+            var message = $"{socketId} said: {Encoding.UTF8.GetString(buffer, 0, result.Count)}";
+            await SendMessageToAll(message); 
+        }
+    }
+}
